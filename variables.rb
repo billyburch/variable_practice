5c4c20993bf27798c702fe72f4178d5aad55a0f0
Billy Burch <billyburch40@gmail.com>
2023-07-31 14:28:44 -0600

    Add integers

--- variables.rb
+++ variables.rb
@@ -2,3 +2,7 @@ greeting_question = "How are you?"
 favorite_color = "What's your favorite color?"
 favorite_season = "Which season is your favorite?"
 
+age = 43
+birth_month = 12
+birth_year = 1979
+
