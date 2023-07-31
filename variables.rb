fbf2fa814b9a06f2af2aa9b696012d2f40e14116
Billy Burch <billyburch40@gmail.com>
2023-07-31 14:58:46 -0600

    Add reassigned variables

--- variables.rb
+++ variables.rb
@@ -14,3 +14,14 @@ name_billy = true
 live_in_denver = true
 drive_jeep = true
 
+# the name_billy variable is declared and assigned to the boolean value true
+name_billy = true
+
+# if you call the variable, the value stored in the variable (true) will be returned
+name_billy
+
+# the name_billy variable is reassigned to the boolean value false
+name_billy = false
+
+#if you call name_billy, the new value stored in that variable will be returned (false)
+name_billy
